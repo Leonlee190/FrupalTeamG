c6d6d11b14f110534a741a29e1eb38e4a99d4ee0
JamesCR <jcc6@pdx.edu>
2017-11-08 14:43:39 -0800

    Refreshed data, commented out based on new information.

--- cgi-location.c
+++ cgi-location.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <cgi.h>
 
-s_cgi *cgi;
-char x_var[4]; //no reasonable reason for 1000+ tile map
-char y_var[4];
-int x_gem = 127;
-int y_gem = 127;
+//commented out due to map file
+//#include <cgi.h>
+//s_cgi *cgi;
+//char x_var[4];
+//char y_var[4];
+//int x_gem = 127;
+//int y_gem = 127;
 
-//simple comparison that sends out the damn info
+//simple comparison that checks for if line input is the location of the diamonds.
 
 void main(void)
 {
 	cgi = cgiInit();
-	//test commit line
 	strcpy(x_var, cgiGetValue(cgi, "x_char"));
 	strcpy(y_var, cgiGetValue(cgi, "y_char"));
 	int x = atoi(x_var);
