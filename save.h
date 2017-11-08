edfdb63da88baeb041a56ed7bf6715d9fe03bcb6
Lydeon <leonlee190@gmail.com>
2017-11-08 14:47:18 -0800

    Update save.h

--- save.h
+++ save.h
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <iostream>
-#include <fstream>
-using namespace std;
+//deleted these library because C can't recognize iostream, fstream, namespace std;
+//#include <iostream>
+//#include <fstream>
+//using namespace std;
 
-int savePlayer(int xCoord, int yCoord, int energy, int whiffles);
+int savePlayer(int MAX, int xCoord, int yCoord, int energy, int whiffles);
 int saveI_help(char * fName, int itemName, int itemNum);
 int saveInventory(int * bag, int length);
 int saveMap();
