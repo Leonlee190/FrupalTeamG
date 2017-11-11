5f3399803fc759bd8d853a06c5b962ac4dba1d0f
Gabriel Miller <gabers.gabers@gamil.com>
2017-11-11 12:00:03 -0800

    Added html file
    Now correctly shows/hides inventory

--- Gabe/PlayerInventory.c
+++ Gabe/PlayerInventory.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//#include <cgi.h>
+#include <cgi.h>
 #include <string.h>
 
 enum item{hatchet = 0, axe = 1, chainsaw = 2, chisel = 3, sledge = 4,
@@ -7,21 +7,17 @@ enum item{hatchet = 0, axe = 1, chainsaw = 2, chisel = 3, sledge = 4,
 
 int inventory[8];
 
-void displayInventory()
-{
-  printf("Content-Type: text/plain;charset=us-ascii\n\n");
-  printf("Hatchets - 15Wh(%d)\n", inventory[hatchet]);
-  printf("Axes - 30Wh(%d)\n", inventory[axe]);
-  printf("Chainsaws - 60Wh(%d)\n", inventory[chainsaw]);
-  printf("Chisels - 5Wh(%d)\n", inventory[chisel]);
-  printf("Sledges - 25Wh(%d)\n", inventory[sledge]);
-  printf("Jackhammers - 100Wh(%d)\n", inventory[jackhammer]);
-  printf("Machetes - 25Wh(%d)\n", inventory[machete]);
-  printf("Shears - 35Wh(%d)\n", inventory[shears]);
-  return 0;
-}
-
 int main(void)
 {
-  
+  printf("Content-Type: text/html;charset=us-ascii\n\n");
+
+  printf("Hatchets - 15Wh   (%d)</br>", inventory[hatchet]);
+  printf("Axes - 30Wh   (%d)</br>", inventory[axe]);
+  printf("Chainsaws - 60Wh   (%d)</br>", inventory[chainsaw]);
+  printf("Chisels - 5Wh   (%d)</br>", inventory[chisel]);
+  printf("Sledges - 25Wh   (%d)</br>", inventory[sledge]);
+  printf("Jackhammers - 100Wh   (%d)</br>", inventory[jackhammer]);
+  printf("Machetes - 25Wh   (%d)</br>", inventory[machete]);
+  printf("Shears - 35Wh   (%d)</br>", inventory[shears]);
+  return 0;
 }
