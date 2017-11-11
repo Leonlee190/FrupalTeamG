fe4910e51b14495bfcf3adea23b27273d4f0b11a
JamesCR <jcc6@pdx.edu>
2017-11-11 12:50:27 -0800

    Updated to recieve map.h data

--- JamesCR/diamond_finder.h
+++ JamesCR/diamond_finder.h
@@ -3,17 +3,13 @@
 #define _GEM
 #include <stdio.h>
 #include <stdlib.h>
+#include "map.h"
 
 /*
 int pull_info_player(int x_play, int y_play);
 int pull_info_diamond(int x_gem, int y_gem);
 int onDiamond(int gem_x, int gem_y, int user_x, int user_y)
-*/
+*/;
 
-int x_diamond;
-int y_diamond;
-
-int onDiamond(int x, int y);
-int pull_diamond();
-
+int onDiamond(int x, int y, map inMap);
 #endif
