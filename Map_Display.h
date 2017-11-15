4fab48069f8bebe2058424b08b2cb59a00ba2fcb
Lydeon <leonlee190@gmail.com>
2017-11-15 01:32:04 -0800

    Add files via upload

---
+++ Map_Display.h
@@ -0,0 +1,26 @@
+/*
+ * CS300: Frupal Project
+ * Team G
+ * Written by: Alexander Mason
+ * File: Map_Display.h
+ */
+#ifndef _MAP_DISPLAY
+#define _MAP_DISPLAY
+
+#include <cgi.h>
+#include <stdbool.h>
+#include "map.h"
+#include "player.h"
+
+//Update the visibility squares
+void setVisible(int pX, int pY, struct map updateMap);
+
+//Draw the html displaying the map
+void drawMap(int pX, int pY, struct map updatedMap);
+
+//Draw the objects on z-index 2
+void drawObjects(int xCoor, int yCoor, struct map updatedMap);
+
+//Compare player location with passed in coordinates
+bool comparePlayerToCoor(int cellX, int cellY, int pX, int pY);
+#endif
--- moves.c
+++ moves.c
@@ -2,7 +2,7 @@
 
 void dir_move(char input, int *fx, int *fy, struct player hero){
 
-	if(input == 'N' || input == 'w' || input == 'W'){
+    if(input == 'd' || input == 'D'){
         if(hero.y < (hero.max - 1)){
             *fx = hero.x;
             *fy = hero.y + 1;
@@ -15,7 +15,7 @@ void dir_move(char input, int *fx, int *fy, struct player hero){
         }
     }
 
-    else if(input == 'S' || input == 's'){
+    else if(input == 'a' || input == 'A'){
         if(hero.y > 0){
             *fx = hero.x;
             *fy = hero.y - 1;
@@ -28,7 +28,7 @@ void dir_move(char input, int *fx, int *fy, struct player hero){
         }
     }
 
-    else if(input == 'E' || input == 'd' || input == 'D'){
+    else if(input == 's' || input == 'S'){
         if(hero.x < (hero.max - 1)){
             *fy = hero.y;
             *fx = hero.x + 1;
@@ -41,7 +41,7 @@ void dir_move(char input, int *fx, int *fy, struct player hero){
         }
     }
 
-    else if(input == 'W' || input == 'a' || input == 'A'){
+    else if(input == 'w' || input == 'W'){
         if(hero.x > 0){
             *fy = hero.y;
             *fx = hero.x - 1;
@@ -62,49 +62,49 @@ void dir_move(char input, int *fx, int *fy, struct player hero){
 }
 
 void obs_check(int x, int y, struct map q){
-	struct player z = load();
-	
-	--z.energy;
-	
-	if(q.cells[x][y].terrain == 0){
-		printf("Walking through a Meadow<br><br>");
-		z.x = x;
-		z.y = y;
-		save(z);
-	}
-	
-	else if(q.cells[x][y].terrain == 1){
-		printf("Walking through a Forest<br><br>");
-		z.x = x;
-		z.y = y;
-		save(z);
-	}
-	
-	else if(q.cells[x][y].terrain == 2){
-		printf("Can't walk on Water<br><br>");
-		save(z);
-	}
-	
-	else if(q.cells[x][y].terrain == 3){
-		printf("Can't walk through Walls<br><br>");
-		save(z);
-	}
-	
-	else if(q.cells[x][y].terrain == 4){
-		printf("Walking through a Bog, lost two energy<br><br>");
-		z.x = x;
-		z.y = y;
-		--z.energy;
-		save(z);
-	}
-	
-	else if(q.cells[x][y].terrain == 5){
-		printf("Walking through a Swamp, lost two energy<br><br>");
-		z.x = x;
-		z.y = y;
-		--z.energy;
-		save(z);
-	}
+    struct player z = load();
+
+    --z.energy;
+
+    if(q.cells[x][y].terrain == 0){
+        printf("Walking through a Meadow<br><br>");
+        z.x = x;
+        z.y = y;
+        save(z);
+    }
+
+    else if(q.cells[x][y].terrain == 1){
+        printf("Walking through a Forest<br><br>");
+        z.x = x;
+        z.y = y;
+        save(z);
+    }
+
+    else if(q.cells[x][y].terrain == 2){
+        printf("Can't walk on Water<br><br>");
+        save(z);
+    }
+
+    else if(q.cells[x][y].terrain == 3){
+        printf("Can't walk through Walls<br><br>");
+        save(z);
+    }
+
+    else if(q.cells[x][y].terrain == 4){
+        printf("Walking through a Bog, lost two energy<br><br>");
+        z.x = x;
+        z.y = y;
+        --z.energy;
+        save(z);
+    }
+
+    else if(q.cells[x][y].terrain == 5){
+        printf("Walking through a Swamp, lost two energy<br><br>");
+        z.x = x;
+        z.y = y;
+        --z.energy;
+        save(z);
+    }
 }
 
 void item_check(int x, int y, struct map q){
@@ -233,7 +233,7 @@ void item_check(int x, int y, struct map q){
         }
 
     }
-    
+
     else if(!strcmp(q.cells[x][y].item, "Bush")){
         if(z.tool[6] && !i){
             printf("<br>Used Machete to cut down Blackberry Bush! Used 2 energy and 25 Wh!<br><br>");
